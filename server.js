4e80c56728df1730c0dee08bb0556e741045675b
vic3king <vic3coorp@gmail.com>
2018-11-16 09:51:53 +0000

    Project setup (express,mustach)

---
+++ server.js
@@ -0,0 +1,28 @@
+//load in dependencies
+const express = require('express')
+
+//template veiw engine
+const mustacheExpress = require('mustache-express')
+require('dotenv').config()
+const port = process.env.PORT
+
+const app = express()
+
+//view engine config
+const mustache = mustacheExpress()
+mustache.cache = null
+app.engine('mustache', mustache)
+app.set('view engine', 'mustache')
+
+//middleware
+app.use(express.static('public'))
+
+//route
+app.get('/list', (req, res) => {
+  res.render('list')
+})
+
+//port
+app.listen(port, () => {
+  console.log(`SERVER started on port ${port}`)
+})
