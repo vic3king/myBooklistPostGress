f40cda23195db0e8078606167a316db981d6085d
vic3king <vic3coorp@gmail.com>
2018-11-17 13:01:36 +0000

    create post route/book/add sql INSERT

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //load in dependencies
 const express = require('express')
+const bodyParser = require('body-parser')
+const { Pool, Client } = require('pg')
 
 //template veiw engine
 const mustacheExpress = require('mustache-express')
+//sets env dependencies
 require('dotenv').config()
 const port = process.env.PORT
 
@@ -16,16 +19,36 @@ app.set('view engine', 'mustache')
 
 //middleware
 app.use(express.static('public'))
+//include body parser to the app so express can read and understand our data
+app.use(bodyParser.urlencoded({ extended: false }))
 
-//route
+//route for list of all books
 app.get('/list', (req, res) => {
   res.render('list')
 })
 
-app.get('/book-form', (req, res) => {
+//route for adding book
+app.get('/book/add', (req, res) => {
   res.render('book-form')
 })
 
+//post a book to the db
+app.post('/book/add', (req, res) => {
+  //another way to connect just for learning sake
+  const client = new Client
+  client.connect()
+    .then(() => {
+      console.log('connection complete')
+      const sql = 'INSERT INTO books (title, authors) VALUES($1, $2)'
+      const params = [req.body.title, req.body.authors]
+      return client.query(sql, params)
+    })
+    .then((result) => {
+      console.log('result?', result)
+      res.redirect('/list')
+    }).catch(err => console.error(err));
+
+})
 
 //port
 app.listen(port, () => {
